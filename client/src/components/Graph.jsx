e9bfc5130d995b44245a32f13915f6bce35790bf
Raymond Hunce <hunce18c@gmail.com>
2021-02-09 21:09:45 -0500

    Implement basic Graph Component

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import zingchart from 'zingchart/es6';
+import ZingChart from 'zingchart-react';
 
-function Graph() {
-  return (
-    <div>
-      <p>GRAPH</p>
-    </div>
-  )
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      config: {
+        type: 'line',
+        'scale-x': {
+          labels: []
+        },
+        series: [
+          {
+            values: []
+          }
+        ]
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.selectedStockDates !== prevProps.selectedStockDates) {
+      this.setState({
+        config: {
+          type: 'line',
+          'scale-x': {
+            labels: this.props.selectedStockDates
+          },
+          series: [
+            {
+              values: this.props.selectedStockPrices
+            }
+          ]
+        }
+      })
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <ZingChart data={this.state.config}/>
+      </div>
+    );
+  }
 }
 
 export default Graph;
