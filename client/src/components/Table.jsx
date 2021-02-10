ff12bc21518439adf49a756c77c1444ee2727937
Raymond Hunce <hunce18c@gmail.com>
2021-02-10 13:24:48 -0500

    Implement basic Table Component

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -4,8 +4,13 @@ class Table extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      targetPriceToEarnings: null,
+      targetPriceToBook: null,
+      targetReturnOnAssets: null,
+      targetReturnOnEquity: null
     }
+
+    this.targetValueChangeHandler = this.targetValueChangeHandler.bind(this);
   }
 
   componentDidMount() {}
@@ -16,39 +21,89 @@ class Table extends React.Component {
   //   }
   // }
 
+  targetValueChangeHandler(e) {
+    this.setState({
+      [e.target.name]: parseFloat(e.target.value)
+    });
+  }
+
   render () {
     return (
       <div>
         <table>
           <tr>
             <th></th>
-            <th>Target</th>
             <th>Current</th>
+            <th>Target</th>
             <th>Go/No-Go</th>
           </tr>
           <tr>
-            <td>P/E Ratio</td>
-            <td><input type="text"></input></td>
-            <td>{this.props.priceToEarningsRatio}</td>
-            <td style={{background: "green"}}></td>
+            <td>P/E Ratio &#40;Current &le; Target ?&#41;</td>
+            <td>{this.props.priceToEarningsRatio || 0.00.toFixed(2)}</td>
+            <td>
+              <input
+                type="text"
+                id="pe-ratio"
+                name="targetPriceToEarnings"
+                onChange={this.targetValueChangeHandler}>
+              </input>
+            </td>
+            <td
+              style={this.state.targetPriceToEarnings >= this.props.priceToEarningsRatio ?
+              {background: "green"} :
+              {background: "red"}}>
+            </td>
           </tr>
           <tr>
-            <td>P/B Ratio</td>
-            <td><input type="text"></input></td>
-            <td>{this.props.priceToBookValueRatio}</td>
-            <td style={{background: "green"}}></td>
+            <td>P/B Ratio &#40;Current &le; Target ?&#41;</td>
+            <td>{this.props.priceToBookValueRatio || 0.00.toFixed(2)}</td>
+            <td>
+              <input
+                type="text"
+                id="pb-ratio"
+                name="targetPriceToBook"
+                onChange={this.targetValueChangeHandler}>
+              </input>
+            </td>
+            <td
+              style={this.state.targetPriceToBook >= this.props.priceToBookValueRatio ?
+              {background: "green"} :
+              {background: "red"}}>
+            </td>
           </tr>
           <tr>
-            <td>ROA</td>
-            <td><input type="text"></input></td>
-            <td>{this.props.returnOnAssets}</td>
-            <td style={{background: "green"}}></td>
+            <td>ROA &#40;%&#41;  &#40;Current &ge; Target ?&#41;</td>
+            <td>{(this.props.returnOnAssets * 100).toFixed(2)}</td>
+            <td>
+              <input
+                type="text"
+                id="roa"
+                name="targetReturnOnAssets"
+                onChange={this.targetValueChangeHandler}>
+              </input>
+            </td>
+            <td
+              style={this.state.targetReturnOnAssets <= (this.props.returnOnAssets * 100) ?
+              {background: "green"} :
+              {background: "red"}}>
+            </td>
           </tr>
           <tr>
-            <td>ROE</td>
-            <td><input type="text"></input></td>
-            <td>{this.props.returnOnEquity}</td>
-            <td style={{background: "green"}}></td>
+            <td>ROE &#40;%&#41;  &#40;Current &ge; Target ?&#41;</td>
+            <td>{(this.props.returnOnEquity * 100).toFixed(2)}</td>
+            <td>
+              <input
+                type="text"
+                id="roe"
+                name="targetReturnOnEquity"
+                onChange={this.targetValueChangeHandler}>
+              </input>
+            </td>
+            <td
+              style={this.state.targetReturnOnEquity <= (this.props.returnOnEquity * 100) ?
+              {background: "green"} :
+              {background: "red"}}>
+            </td>
           </tr>
         </table>
       </div>
