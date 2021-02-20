19ae4b142fd906cd203d3a297342c93fe6fea547
Raymond Hunce <hunce18c@gmail.com>
2021-02-19 21:31:45 -0500

    Minor syntax changes

--- client/src/components/SavedStocks.jsx
+++ client/src/components/SavedStocks.jsx
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 function SavedStocks(props) {
   return (
     <div>
-      <p>SAVED STOCKS</p>
+      <p>SAVED STONKS</p>
       {props.savedStocks.map((stock) => {
         if (typeof stock === 'object' && stock !== null) {
           stock = stock.stock;
