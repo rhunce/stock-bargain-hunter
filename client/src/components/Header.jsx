3e44fab201f8ca9c1429c96d6899b99ab176396a
Raymond Hunce <hunce18c@gmail.com>
2021-02-19 21:44:11 -0500

    Change header image

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div>
       <p>STOCK BARGAIN HUNTER</p>
-      <img src="https://www.usnews.com/dims4/USNEWS/85cf3cc/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fd8%2F17%2F754293f84bc69ddfbffaa5793cae%2F190411-stockmarket-stock.jpg" width="288" height="162" />
+      <img src="https://g.foolcdn.com/editorial/images/494749/gettyimages-523155194.jpg" width="288" height="162" />
     </div>
   )
 }
